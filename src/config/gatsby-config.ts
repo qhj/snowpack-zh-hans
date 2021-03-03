99c636a2176229e7d09d837833f03344bcd4e315
qhj <me@qhj.moe>
2021-03-03 23:22:13 +0800

    add markdown style

--- src/config/gatsby-config.ts
+++ src/config/gatsby-config.ts
@@ -14,6 +14,23 @@ const gatsbyConfig: GatsbyConfig = {
     },
     {
       resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-classes',
+            options: {
+              classMap: {
+                'heading[depth=2]': 'text-3xl font-bold mt-8 mb-5',
+                'heading[depth=3]': 'text-2xl font-bold mt-8 mb-5',
+                'heading[depth=4]': 'text-xl font-bold mb-2',
+                paragraph: 'my-3 leading-6',
+                'list[ordered=false]': 'list-disc pl-8',
+                'list[ordered=true]': 'list-decimal pl-8',
+              },
+            },
+          },
+        ],
+      },
     },
     'gatsby-plugin-postcss',
     'gatsby-plugin-react-helmet',
